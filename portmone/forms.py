6a62ee1e30fc946b00cf4f3480ec0381e81392df
Ivan Petukhov <satels@gmail.com>
2019-10-22 13:27:13 +0300

    request form

--- portmone/forms.py
+++ portmone/forms.py
@@ -4,3 +4,9 @@ from django import forms
 class ResultForm(forms.Form):
 
     data = forms.CharField()
+
+
+class RequestForm(forms.Form):
+
+    bodyRequest = forms.CharField()
+    typeRequest = forms.CharField(initial='json')
