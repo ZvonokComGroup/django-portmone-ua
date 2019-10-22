7dd2bbc996920d500cf3911da936d060928ae07f
Ivan Petukhov <satels@gmail.com>
2019-10-22 12:15:59 +0300

    continue settings

--- portmone/settings.py
+++ portmone/settings.py
@@ -1,7 +1,20 @@
+from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.utils.functional import lazy
 
 
-PAYEE_ID = ''
-SHOP_SITE_ID = ''
-SUCCESS_URL = ''
-FAILURE_URL = ''
-LOGIN = ''
+def get_url(name):
+    return 'https://{}{}'.format(get_current_site(request=None), reverse(name))
+
+
+URL = getattr(settings, 'PORTMONE_GATEWAY_URL', 'https://www.portmone.com.ua/gateway/')
+PAYEE_ID = getattr(settings, 'PORTMONE_PAYEE_ID')
+SHOP_SITE_ID = getattr(settings, 'PORTMONE_SHOP_SITE_ID')
+SUCCESS_URL = getattr(settings, 'PORTMONE_SUCCESS_URL',
+                      lazy(lambda: get_url('intellectmoney-success'), str),
+                      )
+FAILURE_URL = getattr(settings, 'PORTMONE_FAIL_URL',
+                      lazy(lambda: get_url('intellectmoney-fail'), str),
+                      )
+LOGIN = getattr(settings, 'PORTMONE_LOGIN')
