2294d60d777ac94d7e47496579605da165a70991
Ivan Petukhov <satels@gmail.com>
2020-02-12 17:19:09 +0300

    fix SUCCESS_URL and FAILURE_URL

--- portmone/settings.py
+++ portmone/settings.py
@@ -13,10 +13,10 @@ PAYEE_ID = getattr(settings, 'PORTMONE_PAYEE_ID')
 SHOP_SITE_ID = getattr(settings, 'PORTMONE_SHOP_SITE_ID')
 SUCCESS_URL = getattr(settings, 'PORTMONE_SUCCESS_URL',
                       lazy(lambda: get_url('portmone-success'), str),
-                      )
+                      )()
 FAILURE_URL = getattr(settings, 'PORTMONE_FAIL_URL',
                       lazy(lambda: get_url('portmone-fail'), str),
-                      )
+                      )()
 LOGIN = getattr(settings, 'PORTMONE_LOGIN', '')
 CHECK_IP_ENABLED = getattr(settings, 'PORTMONE_CHECK_IP_ENABLED', True)
 IP_LIST = getattr(settings, 'PORTMONE_IP_LIST', [])
